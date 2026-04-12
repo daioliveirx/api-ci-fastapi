eac0448d0f2699c55e013802e137df6d057769f6
Daiana Oliveira <rcrdaiana2003@gmail.com>
2026-04-12 17:06:44 -0300

    feat: adiciona rota multiplicar

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-return {"message": "Hello World"}
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"msg": "API funcionando!"}
+
+@app.get("/somar/{a}/{b}")
+def somar(a: int, b: int):
+    return {"resultado": a + b}
