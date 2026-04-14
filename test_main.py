e8f0125cf06af0eb356912fb0c622a34305bd2cc
Daiana Oliveira <110046138+daioliveirx@users.noreply.github.com>
2026-04-13 21:19:39 -0300

    Fix test assertion for multiplicar endpoint

--- test_main.py
+++ test_main.py
@@ -26,4 +26,4 @@ def test_criar_usuario():
 
 def test_multiplicar():
     response = client.get("/multiplicar/2/2")
-    assert response.json()["resultado"] == 5
+    assert response.json()["resultado"] == 4
